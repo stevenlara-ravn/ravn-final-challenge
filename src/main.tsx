4c815afad0008dec2e9cc4d8cc2f57911367a09b
Steven Lara <stevencarcamo@ravn.co>
2024-12-26 12:09:13 -0600

    style: remove styles from main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
+import AppLayout from "@/core/layout/AppLayout";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles/index.css";
-import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <AppLayout />
+  </StrictMode>
 );
