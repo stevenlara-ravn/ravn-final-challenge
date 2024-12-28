390fb7feb57bb27c54bbd2db46e1b38832a14ea5
Steven Lara <stevencarcamo@ravn.co>
2024-12-28 17:01:03 -0600

    chore: setup env vars

---
+++ src/config/env.ts
@@ -0,0 +1,13 @@
+import { envSchema } from "@/schemas/env";
+
+const { success, error, data } = envSchema.safeParse(import.meta.env);
+
+if (!success) {
+    console.error("❌ Invalid environment variables:", error?.format());
+    throw new Error("Invalid environment variables");
+}
+
+export const {
+    VITE_GRAPHQL_RAVN_API,
+    VITE_GRAPHQL_RAVN_API_KEY,
+} = data;
