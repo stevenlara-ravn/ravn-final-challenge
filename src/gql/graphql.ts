64ad6638e270401b8a9b31bf3ce759c230191033
Steven Lara <stevencarcamo@ravn.co>
2024-12-28 17:21:59 -0600

    chore: generate codegen-ts queries

---
+++ src/gql/graphql.ts
@@ -0,0 +1,155 @@
+/* eslint-disable */
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
+  /** A date-time string at UTC, such as 2019-12-03T09:54:33Z, compliant with the date-time format. */
+  DateTime: { input: any; output: any; }
+};
+
+export type CreateTaskInput = {
+  assigneeId?: InputMaybe<Scalars['String']['input']>;
+  dueDate: Scalars['DateTime']['input'];
+  name: Scalars['String']['input'];
+  pointEstimate: PointEstimate;
+  status: Status;
+  tags: Array<TaskTag>;
+};
+
+export type DeleteTaskInput = {
+  id: Scalars['String']['input'];
+};
+
+export type FilterTaskInput = {
+  assigneeId?: InputMaybe<Scalars['String']['input']>;
+  dueDate?: InputMaybe<Scalars['DateTime']['input']>;
+  name?: InputMaybe<Scalars['String']['input']>;
+  ownerId?: InputMaybe<Scalars['String']['input']>;
+  pointEstimate?: InputMaybe<PointEstimate>;
+  status?: InputMaybe<Status>;
+  tags?: InputMaybe<Array<TaskTag>>;
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  createTask: Task;
+  deleteTask: Task;
+  updateTask: Task;
+};
+
+
+export type MutationCreateTaskArgs = {
+  input: CreateTaskInput;
+};
+
+
+export type MutationDeleteTaskArgs = {
+  input: DeleteTaskInput;
+};
+
+
+export type MutationUpdateTaskArgs = {
+  input: UpdateTaskInput;
+};
+
+/** Estimate point for a task */
+export enum PointEstimate {
+  Eight = 'EIGHT',
+  Four = 'FOUR',
+  One = 'ONE',
+  Two = 'TWO',
+  Zero = 'ZERO'
+}
+
+export type Query = {
+  __typename?: 'Query';
+  profile: User;
+  tasks: Array<Task>;
+  users: Array<User>;
+};
+
+
+export type QueryTasksArgs = {
+  input: FilterTaskInput;
+};
+
+/** Status for Task */
+export enum Status {
+  Backlog = 'BACKLOG',
+  Cancelled = 'CANCELLED',
+  Done = 'DONE',
+  InProgress = 'IN_PROGRESS',
+  Todo = 'TODO'
+}
+
+export type Task = {
+  __typename?: 'Task';
+  assignee?: Maybe<User>;
+  createdAt: Scalars['DateTime']['output'];
+  creator: User;
+  dueDate: Scalars['DateTime']['output'];
+  id: Scalars['ID']['output'];
+  name: Scalars['String']['output'];
+  pointEstimate: PointEstimate;
+  position: Scalars['Float']['output'];
+  status: Status;
+  tags: Array<TaskTag>;
+};
+
+/** Enum for tags for tasks */
+export enum TaskTag {
+  Android = 'ANDROID',
+  Ios = 'IOS',
+  NodeJs = 'NODE_JS',
+  Rails = 'RAILS',
+  React = 'REACT'
+}
+
+export type UpdateTaskInput = {
+  assigneeId?: InputMaybe<Scalars['String']['input']>;
+  dueDate?: InputMaybe<Scalars['DateTime']['input']>;
+  id: Scalars['String']['input'];
+  name?: InputMaybe<Scalars['String']['input']>;
+  pointEstimate?: InputMaybe<PointEstimate>;
+  position?: InputMaybe<Scalars['Float']['input']>;
+  status?: InputMaybe<Status>;
+  tags?: InputMaybe<Array<TaskTag>>;
+};
+
+export type User = {
+  __typename?: 'User';
+  avatar?: Maybe<Scalars['String']['output']>;
+  createdAt: Scalars['DateTime']['output'];
+  email: Scalars['String']['output'];
+  fullName: Scalars['String']['output'];
+  id: Scalars['ID']['output'];
+  type: UserType;
+  updatedAt: Scalars['DateTime']['output'];
+};
+
+/** Type of the User */
+export enum UserType {
+  Admin = 'ADMIN',
+  Candidate = 'CANDIDATE'
+}
+
+export type TasksQueryVariables = Exact<{
+  input: FilterTaskInput;
+}>;
+
+
+export type TasksQuery = { __typename?: 'Query', tasks: Array<{ __typename?: 'Task', createdAt: any, dueDate: any, id: string, name: string, pointEstimate: PointEstimate, position: number, status: Status, tags: Array<TaskTag>, assignee?: { __typename?: 'User', avatar?: string | null, createdAt: any, email: string, fullName: string, id: string, type: UserType, updatedAt: any } | null, creator: { __typename?: 'User', avatar?: string | null, createdAt: any, email: string, fullName: string, id: string, type: UserType, updatedAt: any } }> };
+
+
+export const TasksDocument = { "kind": "Document", "definitions": [{ "kind": "OperationDefinition", "operation": "query", "name": { "kind": "Name", "value": "Tasks" }, "variableDefinitions": [{ "kind": "VariableDefinition", "variable": { "kind": "Variable", "name": { "kind": "Name", "value": "input" } }, "type": { "kind": "NonNullType", "type": { "kind": "NamedType", "name": { "kind": "Name", "value": "FilterTaskInput" } } } }], "selectionSet": { "kind": "SelectionSet", "selections": [{ "kind": "Field", "name": { "kind": "Name", "value": "tasks" }, "arguments": [{ "kind": "Argument", "name": { "kind": "Name", "value": "input" }, "value": { "kind": "Variable", "name": { "kind": "Name", "value": "input" } } }], "selectionSet": { "kind": "SelectionSet", "selections": [{ "kind": "Field", "name": { "kind": "Name", "value": "assignee" }, "selectionSet": { "kind": "SelectionSet", "selections": [{ "kind": "Field", "name": { "kind": "Name", "value": "avatar" } }, { "kind": "Field", "name": { "kind": "Name", "value": "createdAt" } }, { "kind": "Field", "name": { "kind": "Name", "value": "email" } }, { "kind": "Field", "name": { "kind": "Name", "value": "fullName" } }, { "kind": "Field", "name": { "kind": "Name", "value": "id" } }, { "kind": "Field", "name": { "kind": "Name", "value": "type" } }, { "kind": "Field", "name": { "kind": "Name", "value": "updatedAt" } }] } }, { "kind": "Field", "name": { "kind": "Name", "value": "createdAt" } }, { "kind": "Field", "name": { "kind": "Name", "value": "creator" }, "selectionSet": { "kind": "SelectionSet", "selections": [{ "kind": "Field", "name": { "kind": "Name", "value": "avatar" } }, { "kind": "Field", "name": { "kind": "Name", "value": "createdAt" } }, { "kind": "Field", "name": { "kind": "Name", "value": "email" } }, { "kind": "Field", "name": { "kind": "Name", "value": "fullName" } }, { "kind": "Field", "name": { "kind": "Name", "value": "id" } }, { "kind": "Field", "name": { "kind": "Name", "value": "type" } }, { "kind": "Field", "name": { "kind": "Name", "value": "updatedAt" } }] } }, { "kind": "Field", "name": { "kind": "Name", "value": "dueDate" } }, { "kind": "Field", "name": { "kind": "Name", "value": "id" } }, { "kind": "Field", "name": { "kind": "Name", "value": "name" } }, { "kind": "Field", "name": { "kind": "Name", "value": "pointEstimate" } }, { "kind": "Field", "name": { "kind": "Name", "value": "position" } }, { "kind": "Field", "name": { "kind": "Name", "value": "status" } }, { "kind": "Field", "name": { "kind": "Name", "value": "tags" } }] } }] } }] } as unknown as DocumentNode<TasksQuery, TasksQueryVariables>;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import Router from "@/routes/Router";
+import { client } from "@/services/apollo";
+import { ApolloProvider } from "@apollo/client";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Router />
+    <ApolloProvider client={client}>
+      <Router />
+    </ApolloProvider>
   </StrictMode>,
 );
