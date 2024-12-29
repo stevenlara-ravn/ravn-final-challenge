395da461389b30a1cea24e3b3cfe043be1edb197
Steven Lara <stevencarcamo@ravn.co>
2024-12-29 01:57:53 -0600

    chore: add utilities

---
+++ src/modules/core/constants/tag-colors.ts
@@ -0,0 +1,7 @@
+export const deviceTagColors = {
+    "IOS": "bg-ravn-secondary-4/10 text-ravn-secondary-4",
+    "ANDROID": "bg-ravn-tertiary-4/10 text-ravn-tertiary-4",
+    "RAILS": "bg-ravn-primary-4/10 text-ravn-primary-4",
+    "REACT": "bg-ravn-blue-1/10 text-ravn-blue-1",
+    "NODE_JS": "bg-ravn-secondary-4/10 text-ravn-secondary-4",
+};
---
+++ src/utils/text-transform.ts
@@ -0,0 +1,2 @@
+export const normalizeText = (text: string) => text.toLowerCase().replace(/_/g, " ");
+export const zeroPad = (number: number) => number.toString().padStart(2, "0");
