303be7d39ded7702a3781973a457da1750c41e53
Steven Lara <stevencarcamo@ravn.co>
2024-12-30 08:09:42 -0600

    style: add font and reorganize them

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -36,5 +38,137 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    /** @type {import('tailwindcss/types/config').PluginCreator} */
+    ({ addUtilities }) => {
+      addUtilities({
+        ".text-display-xl-regular": {
+          "fontSize": "64px",
+          "lineHeight": "88px",
+          "letterSpacing": "1px",
+          "fontWeight": "400",
+          "fontFamily": "\"SF Compact Display\", sans-serif"
+        },
+        ".text-display-l-regular": {
+          "fontSize": "56px",
+          "lineHeight": "72px",
+          "letterSpacing": "1px",
+          "fontWeight": "400",
+          "fontFamily": "\"SF Pro Display\", sans-serif"
+        },
+        ".text-display-m-regular": {
+          "fontSize": "48px",
+          "lineHeight": "56px",
+          "letterSpacing": "1px",
+          "fontWeight": "400",
+          "fontFamily": "\"SF Pro Display\", sans-serif"
+        },
+        ".text-display-s-regular": {
+          "fontSize": "32px",
+          "lineHeight": "48px",
+          "letterSpacing": "1px",
+          "fontWeight": "400",
+          "fontFamily": "\"SF Pro Display\", sans-serif"
+        },
+        ".text-display-xs-regular": {
+          "fontSize": "24px",
+          "lineHeight": "32px",
+          "letterSpacing": "1px",
+          "fontWeight": "400",
+          "fontFamily": "\"SF Pro Display\", sans-serif"
+        },
+        ".text-display-xl-bold": {
+          "fontSize": "64px",
+          "lineHeight": "88px",
+          "letterSpacing": "1px",
+          "fontWeight": "700",
+          "fontFamily": "\"SF Pro Display\", sans-serif"
+        },
+        ".text-display-l-bold": {
+          "fontSize": "56px",
+          "lineHeight": "72px",
+          "letterSpacing": "1px",
+          "fontWeight": "700",
+          "fontFamily": "\"SF Pro Display\", sans-serif"
+        },
+        ".text-display-m-bold": {
+          "fontSize": "48px",
+          "lineHeight": "56px",
+          "letterSpacing": "1px",
+          "fontWeight": "700",
+          "fontFamily": "\"SF Pro Display\", sans-serif"
+        },
+        ".text-display-s-bold": {
+          "fontSize": "32px",
+          "lineHeight": "48px",
+          "letterSpacing": "1px",
+          "fontWeight": "700",
+          "fontFamily": "\"SF Pro Display\", sans-serif"
+        },
+        ".text-display-xs": {
+          "fontSize": "24px",
+          "lineHeight": "32px",
+          "letterSpacing": "1px",
+          "fontWeight": "700",
+          "fontFamily": "\"SF Pro Display\", sans-serif"
+        },
+        ".text-body-x-regular": {
+          "fontSize": "20px",
+          "lineHeight": "32px",
+          "letterSpacing": "0.75px",
+          "fontWeight": "400",
+          "fontFamily": "\"SF Pro Display\", sans-serif"
+        },
+        ".text-body-l-regular": {
+          "fontSize": "18px",
+          "lineHeight": "32px",
+          "letterSpacing": "0.75px",
+          "fontWeight": "400",
+          "fontFamily": "\"SF Pro Display\", sans-serif"
+        },
+        ".text-body-m-regular": {
+          "fontSize": "15px",
+          "lineHeight": "24px",
+          "letterSpacing": "0.75px",
+          "fontWeight": "400",
+          "fontFamily": "\"SF Pro Display\", sans-serif"
+        },
+        ".text-body-s-regular": {
+          "fontSize": "13px",
+          "lineHeight": "22px",
+          "letterSpacing": "0.25px",
+          "fontWeight": "400",
+          "fontFamily": "\"SF Pro Display\", sans-serif"
+        },
+        ".text-body-xl-bold": {
+          "fontSize": "20px",
+          "lineHeight": "32px",
+          "letterSpacing": "0.75px",
+          "fontWeight": "600",
+          "fontFamily": "\"SF Pro Display\", sans-serif"
+        },
+        ".text-body-l-bold": {
+          "fontSize": "18px",
+          "lineHeight": "32px",
+          "letterSpacing": "0.75px",
+          "fontWeight": "600",
+          "fontFamily": "\"SF Pro Display\", sans-serif"
+        },
+        ".text-body-m-bold": {
+          "fontSize": "15px",
+          "lineHeight": "24px",
+          "letterSpacing": "0.75px",
+          "fontWeight": "600",
+          "fontFamily": "\"SF Pro Display\", sans-serif"
+        },
+        ".text-body-s-bold": {
+          "fontSize": "13px",
+          "lineHeight": "22px",
+          "letterSpacing": "0.25px",
+          "fontWeight": "600",
+          "fontFamily": "\"SF Pro Display\", sans-serif"
+        }
+      });
+    }
+  ],
 };
