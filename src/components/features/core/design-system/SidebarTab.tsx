5a385cc674fcd2578fac829b8c14529db29c84d4
Steven Lara <stevencarcamo@ravn.co>
2024-12-31 12:01:01 -0600

    style: add classname utiliy

--- src/components/features/core/design-system/SidebarTab.tsx
+++ src/components/features/core/design-system/SidebarTab.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import { NavLink } from "react-router";
 
 interface SidebarTabProps {
@@ -8,18 +9,22 @@ interface SidebarTabProps {
 
 export default function SidebarTab({ icon, label, url }: SidebarTabProps) {
   return (
-    <NavLink
-      className={({ isActive }: { isActive: boolean }) =>
-        `flex h-[56px] w-full items-center justify-start gap-4 pl-4 text-ravn-neutral-2 hover:text-ravn-primary-4 ${
-          isActive
-            ? "border-r-4 border-ravn-primary-4 bg-gradient-to-r from-[#BA252500] from-10% to-[#D24D4D1A] to-100% text-ravn-primary-4"
-            : ""
-        }`
-      }
-      to={url}
-    >
-      {icon}
-      <p className="uppercase text-body-m-bold">{label}</p>
-    </NavLink>
+    <li className="h-max w-full">
+      <NavLink
+        className={({ isActive }: { isActive: boolean }) =>
+          clsx(
+            "flex h-[56px] w-full items-center justify-start gap-4 pl-4 text-ravn-neutral-2 hover:text-ravn-primary-4",
+            {
+              "border-r-4 border-ravn-primary-4 bg-gradient-to-r from-[#BA252500] from-10% to-[#D24D4D1A] to-100% text-ravn-primary-4":
+                isActive,
+            },
+          )
+        }
+        to={url}
+      >
+        {icon}
+        <p className="uppercase text-body-m-bold">{label}</p>
+      </NavLink>
+    </li>
   );
 }
