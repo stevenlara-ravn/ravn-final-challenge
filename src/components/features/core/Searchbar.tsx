91a70f192623dcca6475b65611d7e3fd8bfed5b9
Steven Lara <stevencarcamo@ravn.co>
2025-01-02 10:58:47 -0600

    style: hide x on input field

--- src/components/features/core/Searchbar.tsx
+++ src/components/features/core/Searchbar.tsx
@@ -1,18 +1,38 @@
 import SearchIcon from "@/assets/icons/search-bar/search.svg?react";
 import XCloseIcon from "@/assets/icons/x-close.svg?react";
+import { useState } from "react";
 
 export default function Searchbar() {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleReset = () => {
+    setInputValue("");
+  };
+
   return (
-    <form className="relative flex h-full w-full items-center justify-between">
+    <div
+      className="relative flex h-full w-full items-center justify-between"
+      onSubmit={(e) => e.preventDefault()}
+    >
       <SearchIcon className="absolute left-6 h-6 w-6 text-ravn-neutral-2" />
+
       <input
-        className="h-full w-full rounded-2xl bg-ravn-neutral-4 py-5 pl-[72px] text-ravn-neutral-2 text-body-m-regular placeholder:text-ravn-neutral-2 focus:outline-none"
+        className="h-full w-full rounded-2xl bg-ravn-neutral-4 py-5 pl-[72px] pr-7 text-ravn-neutral-2 text-body-m-regular placeholder:text-ravn-neutral-2 focus:outline-none"
+        onChange={(e) => setInputValue(e.target.value)}
         placeholder="Search"
         type="search"
+        value={inputValue}
       />
-      <button className="absolute right-6 h-6 w-6" type="reset">
-        <XCloseIcon className="h-full w-full text-ravn-neutral-2" />
-      </button>
-    </form>
+
+      {inputValue.length >= 1 && (
+        <button
+          className="absolute right-6 h-6 w-6"
+          onClick={handleReset}
+          type="reset"
+        >
+          <XCloseIcon className="h-full w-full text-ravn-neutral-2" />
+        </button>
+      )}
+    </div>
   );
 }
--- src/components/features/core/TechTag.tsx
+++ src/components/features/core/TechTag.tsx
@@ -3,18 +3,15 @@ import { deviceTagColors } from "@/constants/colors";
 import { TaskTag } from "@/gql/graphql";
 import ReactProps from "@/types/ReactProps";
 import { normalizeText } from "@/utils/text-transform";
-import clsx from "clsx";
 
 interface TechTagProps extends ReactProps {
-  label: TaskTag | `+${number}`;
+  label: `${TaskTag}` | `+${number}`;
 }
 
 export default function TechTag(props: TechTagProps) {
   return (
     <Tag
-      className={clsx(props.className, {
-        [deviceTagColors[props.label as TaskTag]]: props.label in TaskTag,
-      })}
+      className={`${deviceTagColors[props.label as TaskTag]} ${props.className}`}
     >
       <p className="text-center uppercase text-body-m-bold">
         {normalizeText(props.label)}
