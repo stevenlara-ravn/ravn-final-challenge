abdffa4540b61f35ddb1fb5b8d7b8451f2805fc3
Steven Lara <stevencarcamo@ravn.co>
2025-01-05 22:51:13 -0600

    style: fix task column width

--- src/components/features/task/TaskColumn.tsx
+++ src/components/features/task/TaskColumn.tsx
@@ -9,7 +9,7 @@ interface TaskColumnProps {
 
 export default function TaskColumn({ status, tasks }: TaskColumnProps) {
   return (
-    <section className="flex h-full w-full min-w-[348px] flex-col items-center justify-start gap-4 overflow-hidden rounded-lg">
+    <section className="flex h-full w-[348px] min-w-[348px] flex-col items-center justify-start gap-4 overflow-hidden rounded-lg">
       <p className="h-8 w-full capitalize text-ravn-neutral-1 text-body-l-bold">
         {normalizeText(status)} ({zeroPad(tasks.length ?? 0)})
       </p>
