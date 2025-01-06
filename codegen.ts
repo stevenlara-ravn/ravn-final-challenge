c808c63db299adfa7c187e025ade083aa3b1a666
Steven Lara <stevencarcamo@ravn.co>
2025-01-06 13:18:05 -0600

    chore: install dotenv dep

--- codegen.ts
+++ codegen.ts
@@ -1,8 +1,9 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
+import "dotenv/config";
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "https://syn-api-prod.herokuapp.com/graphql",
+  schema: process.env.VITE_GRAPHQL_RAVN_API as string,
   documents: "src/gql/**/*.graphql",
   generates: {
     "src/gql/graphql.ts": {
